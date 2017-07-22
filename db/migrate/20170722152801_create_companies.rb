138ee1bace0dca2bf310bf7f7e5fa0cd8714e300
Mariely Delacruz <mdlc0502@gmail.com>
2017-07-22 11:51:17 -0400

    made companies scaffold

---
+++ db/migrate/20170722152801_create_companies.rb
@@ -0,0 +1,11 @@
+class CreateCompanies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :companies do |t|
+      t.belongs_to :user, foreign_key: true
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721173843) do
+ActiveRecord::Schema.define(version: 20170722152801) do
+
+  create_table "companies", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
