667b02061cb0e3cac318204ec6fb96f990ee1202
Mariely Delacruz <mdlc0502@gmail.com>
2017-07-22 15:42:04 -0400

    added phone column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722152801) do
+ActiveRecord::Schema.define(version: 20170722172535) do
 
   create_table "companies", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170722152801) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
     t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
