ff7c551ef0e860b887af622712f7a01bd22c0d07
Michael Braun <mikebbraun@gmail.com>
2017-07-22 16:07:30 -0400

    setting up tests

---
+++ spec/features/new_users_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "New users", type: :feature do
+  # pending "add some scenarios (or delete) #{__FILE__}"
+
+  before do
+    visit root_path
+  end
+
+  it 'should let a new user sign up' do
+  end
+
+  it 'should let an existing user sign in' do
+  end
+end
