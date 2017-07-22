6c6ded12546428fe6e73116e6710533ce0f662ff
Michael Braun <mikebbraun@gmail.com>
2017-07-22 16:22:01 -0400

    sign in/up tests

--- spec/features/new_users_spec.rb
+++ spec/features/new_users_spec.rb
@@ -8,8 +8,18 @@ RSpec.feature "New users", type: :feature do
   end
 
   it 'should let a new user sign up' do
+    click_link 'Sign Up Here'
+    fill_in 'Username', :with => 'abc'
+    fill_in 'Password', :with => 'abc123'
+    fill_in 'Password confirmation', :with => 'abc123'
+    click_button 'Create User'
+    # save_and_open_page
   end
 
   it 'should let an existing user sign in' do
+    fill_in 'Username', :with => 'abc'
+    fill_in 'Password', :with => 'abc123'
+    click_button 'Log in'
+    # save_and_open_page
   end
 end
