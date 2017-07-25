e90ca3783eb6f8457afba698cf37bd46ab459163
Mariely Delacruz <mdlc0502@gmail.com>
2017-07-24 20:33:01 -0400

    fixed schema issue

--- db/migrate/20170723162633_create_join_table_company_form.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCompanyForm < ActiveRecord::Migration[5.1]
-  def change
-    create_join_table :companies, :forms do |t|
-      t.index [:company_id, :form_id]
-      t.index [:form_id, :company_id]
-    end
-  end
-end
