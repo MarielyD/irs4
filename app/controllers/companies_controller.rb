e26c3c402076b82934c3805b611a062ed2345463
Michael Braun <mikebbraun@gmail.com>
2017-07-28 21:47:38 -0400

    remove the test

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -32,7 +32,6 @@ class CompaniesController < ApplicationController
         format.html { redirect_to @company, notice: 'Company was successfully created.' }
         format.json { render :show, status: :created, location: @company }
         FollowUpJob.new(@company.email, current_user.username).enqueue
-        UserMailer.follow_up(email, name).deliver_now
 
       else
         format.html { render :new }
