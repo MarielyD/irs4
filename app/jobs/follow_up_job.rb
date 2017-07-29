79f392261c9ba779d28e5769902b0c1985da17ef
Michael Braun <mikebbraun@gmail.com>
2017-07-28 21:26:28 -0400

    follow up job?

--- app/jobs/follow_up_job.rb
+++ app/jobs/follow_up_job.rb
@@ -1,10 +1,10 @@
 class FollowUpJob < ApplicationJob
   queue_as :email
-  include Que::Mailer
 
   def perform(email, name)
     # Do something later
     UserMailer.follow_up(email, name).deliver_now
+    p "<>"*20
     p "email sent"
   end
 end
