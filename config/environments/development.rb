894e514be4da851d6b2ee87e6e8c81cd18b1c97d
Michael Braun <mikebbraun@gmail.com>
2017-07-29 10:08:30 -0400

    config action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     port: 587,
     address: 'smtp.sendgrid.net',
-    user_name: 'apikey',
+    user_name: apikey,
     password: ENV['SEND_GRID_PASSWORD'],
     domain: ENV['SEND_GRID_DOMAIN'],
     authentication: :plain
