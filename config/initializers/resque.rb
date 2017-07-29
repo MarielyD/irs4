2155f9f124e5888f33af7ff97d1a17357e4a50d7
Michael Braun <mikebbraun@gmail.com>
2017-07-28 21:37:51 -0400

    removed option from resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 Resque.redis = ENV['REDIS_URL']
 
 # optional
-Resque.after_fork = Proc.new {
-  ActiveRecord::Base.establish_connection
-}
+# Resque.after_fork = Proc.new {
+#   ActiveRecord::Base.establish_connection
+# }
