c690aa2f39743572160c87e563dce25833adc4ef
Michael Braun <mikebbraun@gmail.com>
2017-07-28 20:12:54 -0400

    commented out duplicate methods

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,8 +3,8 @@ class Company < ApplicationRecord
   has_many :company_forms,  :dependent => :delete_all
   has_many :forms, :through => :company_forms
 
-  after_save :delete_duplicate
-  after_update :delete_duplicate
+  # after_save :delete_duplicate
+  # after_update :delete_duplicate
   private
     def delete_duplicate
       self.company_forms.group(:form_id).having("count(*) > 1").map(&:destroy)
